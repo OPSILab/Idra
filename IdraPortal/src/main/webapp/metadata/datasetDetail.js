ea1f3966d53008c9a0f14a6ac17f238d6e421256
Roberto Callari <robertocallari89@gmail.com>
2018-11-22 14:19:06 +0100

    Updates for Idra version 1.2 (#10)
    * Updated Front END
    
    * Datalet Synch Icon
    
    * FrontEnd updates
    
    * Adding internazionalization (EN - IT)
    
    * Internazionalization Updates
    
    * Fix Municipality Translation
    
    * Responsivness
    
    * Toggle button fix
    
    * Added About section
    
    * Internationalization Administration Functionalities
    
    * Changes Next Release updated
    
    * WordCloud Fix; SVG logo; disable pass update if Fiware
    
    * Modified Screenshots
    
    * Modified changes next release
    
    * Update Changelog
    Merge pull request #1 from OPSILab/master
    
    Updating Fork
    Addedd Related Resource to DCATDataset model
    Persistence Manager Fix
    Updated Front END
    Datalet Synch Icon
    Updates for Incubated Status
    
    * Reformatted Markdown
    * Restructured README according to standard-readme
    * Added additional badges to readme and docs front-page
    * Added code highlights
    * Added FIWARE CSS and favicon
    
    Merge pull request #9 from jason-fox/feature/css-badges-readme
    
    Updates for Incubated Status
    FrontEnd updates
    Merge remote-tracking branch 'origin/frontend-update'
    Merge branch 'master' of https://github.com/robcalla/Idra.git
    Adding internazionalization (EN - IT)
    Internazionalization Updates
    Fix Municipality Translation
    Responsivness
    Toggle button fix
    Added About section
    Fixed docker-compose
    
    Dump path for idra_db.sql fixed and wait-for-it script for DB container
    added
    Internationalization Administration Functionalities
    Changes Next Release updated
    WordCloud Fix; SVG logo; disable pass update if Fiware
    Merge branch 'master' of https://github.com/OPSILab/Idra.git
    Transferring API Description file from Apiary.io
    Modified Screenshots
    Merge branch 'master' of https://github.com/OPSILab/Idra.git
    Modified changes next release

--- IdraPortal/src/main/webapp/metadata/datasetDetail.js
+++ IdraPortal/src/main/webapp/metadata/datasetDetail.js
@@ -55,7 +55,14 @@ angular.module("IdraPlatform").controller('DatasetDetailCtrl',['$scope','$rootSc
 				allowed=false;
 			break;
 			}
+			
+			if(!allowed){
+				if(parameter.includes("csv")){
+					allowed=true;
+				}
+			}
 		}
+		
 		distribution.dataletShowButtonEnabled=allowed;
 	}
 		
@@ -64,6 +71,7 @@ angular.module("IdraPlatform").controller('DatasetDetailCtrl',['$scope','$rootSc
 		$scope.dataset.licenses = [];
 		var tmpLic=[];
 		for(i=0; i<$scope.dataset.distributions.length; i++){
+			$scope.dataset.distributions[i].lockFile=false;
 			$scope.dataset.distributions[i].collapseDetails=true;
 			if(tmpLic.indexOf($scope.dataset.distributions[i].license.name)<0 && $scope.dataset.distributions[i].license.name!=''){
 				tmpLic.push($scope.dataset.distributions[i].license.name);
@@ -199,10 +207,16 @@ angular.module("IdraPlatform").controller('DatasetDetailCtrl',['$scope','$rootSc
 			default:
 				str='file'
 			}
+			
+			if(str=="file"){
+				if(parameter.includes("csv")){
+					str="csv";
+				}
+			}
 		}else{
 			str='file';
 		}
-
+		
 		return 'images/'+str+'.png';
 
 	};
@@ -282,10 +296,13 @@ angular.module("IdraPlatform").controller('DatasetDetailCtrl',['$scope','$rootSc
 				method: 'GET',
 				url: config.CLIENT_SERVICES_BASE_URL+config.CHECK_DISTRIBUTION_URL+window.encodeURIComponent(distribution.downloadURL)
 		};
-			
+		
+		distribution.lockFile=true;
 		$http(reqCheckDownloadUri).then(function(value){
+			distribution.lockFile=false;
 			$window.open($sce.trustAsResourceUrl(config.DATALET_URL+"?format="+parameter+"&nodeID="+nodeID+"&distributionID="+distribution.id+"&datasetID="+datasetID+"&url="+window.encodeURIComponent(distribution.downloadURL)));
 		},function(value){
+			distribution.lockFile=false;
 			distribution.distributionDonwloadUrlOk = false;
 			dialogs.error("Unable to create Datalet","File with url <br/> "+distribution.downloadURL+" <br/> returned "+value.status+"!");
 		});		
