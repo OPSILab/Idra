f4b06b1a921239c3336204339e8f747c333efc8e
Antonino Sirchia <asirchia@gmail.com>
2019-02-19 14:23:42 +0100

    OpenDataSoft support

---
+++ Idra/src/main/java/it/eng/idra/beans/opendatasoft/InnerDatasetFieldAnnotations.java
@@ -0,0 +1,17 @@
+package it.eng.idra.beans.opendatasoft;
+
+import java.util.List;
+
+public class InnerDatasetFieldAnnotations {
+
+	private List<String> facet;
+
+	public List<String> getFacet() {
+		return facet;
+	}
+
+	public void setFacet(List<String> facet) {
+		this.facet = facet;
+	}
+	
+}
