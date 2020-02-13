a38f29619c49709de78d31d41f80cd6d2f8604a3
Roberto Callari <robertocallari89@gmail.com>
2020-02-13 15:17:36 +0100

    Added idraopendata/deep to docker compose

---
+++ docker/deep/configuration.js
@@ -0,0 +1,12 @@
+var configs = {
+	"odfURL" : "http://localhost:8080/",
+	"deepBaseUrl" : "http://localhost:8082/deep/"
+};
+
+function getODFUrl() {
+	return configs.odfURL;
+};
+
+function getDeepBaseUrl() {
+	return configs.deepBaseUrl;
+};
